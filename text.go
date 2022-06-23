190abca07d4c60c7f37b89bb166e9fde5ed8378f
segre <segrehouck5458@gmail.com>
2022-06-23 18:11:29 +0900

    fix テキストの内容

--- text.go
+++ text.go
@@ -18,8 +18,5 @@ func GetText(dt_now time.Time) (text string) {
 	if dt_now.Month() == 3 && dt_now.Day() == 28 && dt_now.Hour() == 9 && dt_now.Minute() == 1{
 		text = "お兄ちゃんお誕生日おめでとう！とっても楽しい1年にしてね！またお兄ちゃんと年齢が離れちゃった。クレーいつになったら追いつけるのかなー"
 	}
-	if dt_now.Hour() == 17{
-		text = "hoge"
-	}
 	return text
 }
